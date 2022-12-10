3345daeebfb552fc68ff9dc7845712a30d703a16
Maya-Blep <108821438+Maya-Blep@users.noreply.github.com>
2022-12-10 14:08:39 -0500

    Add files via upload

---
+++ gift-box/colors.py
@@ -0,0 +1,34 @@
+from sense_hat import SenseHat
+
+
+r = (255, 0, 0)
+g = (0, 255, 0)
+b = (0, 0, 255)
+k = (0, 0, 0)
+w = (255, 255, 255)
+c = (0, 255, 255)
+y = (255, 255, 0)
+o = (255, 128, 0)
+n = (255, 128, 128)
+p = (128, 0, 128)
+d = (255, 0, 128)
+l = (128, 255, 128)
+
+pinkandblue = [k, k, k, k, k, k, k, k,
+             k, b, k, k, k, k, b, k,
+             k, k, b, k, k, b, k, k,
+             k, n, n, b, b, n, n, k,
+             k, b, b, b, b, b, b, k,
+             k, n, n, b, b, n, n, k,
+             k, n, n, b, b, n, n, k,
+             k, k, k, k, k, k, k, k
+]	
+		
+		
+		
+		
+		
+		
+		
+		
+		
---
+++ gift-box/presents.py
@@ -0,0 +1,6 @@
+class Gift:
+    def __init__(self, color, ribbon, box):
+        self.color = color
+        self.ribbon = ribbon
+        self.box = box
+
